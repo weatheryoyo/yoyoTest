bd1469d0bb7816984de2727376e44bb96fb607ba
yangyan <yangyan@cstonline.com>
2016-11-20 22:39:28 +0800

    增加去掉集合中所有指定元素的代码

--- src/main/java/com/yoyo/Test.java
+++ src/main/java/com/yoyo/Test.java
@@ -1,8 +1,28 @@
 package com.yoyo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("a");
-		System.out.println("b");
+		Collection<String> coll = new HashSet<String>();
+		coll.add("asdf");
+		coll.add(null);
+		coll.add("asdf");
+		coll.add(null);
+		coll.add("asdf");
+		for (String string : coll) {
+			System.out.println(string);
+		}
+		List<String> list = new ArrayList<String>();
+		list.add(null);
+		coll.removeAll(list);
+		System.out.println("***************************************");
+		for (String string : coll) {
+			System.out.println(string);
+		}
 	}
 }
